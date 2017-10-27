57003f205f626708be52189af34482951e9207ca
shiShanLing <13673387452@163.com>
2017-10-27 17:43:54 +0800

    任务订单有变更 但是 还没接通  课程价钱有变更 但是 还没接通 

---
+++ cheZhiLianCoach/cheZhiLianCoach/MyOrder/Main/V/TaskHeadView.m
@@ -0,0 +1,21 @@
+//
+//  TaskHeadView.m
+//  cheZhiLianCoach
+//
+//  Created by 石山岭 on 2017/10/27.
+//  Copyright © 2017年 石山岭. All rights reserved.
+//
+
+#import "TaskHeadView.h"
+
+@implementation TaskHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
